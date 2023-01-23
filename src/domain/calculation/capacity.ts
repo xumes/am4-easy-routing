0b4ac31609af2e420454b78fc2ff7b4e97d3bbac
Reginaldo Santos <reginaldo.santos@redspace.com>
2023-01-23 16:09:51 -0400

    feat: ensure Capacity returns error when null or undefined data is provided

--- src/domain/calculation/capacity.ts
+++ src/domain/calculation/capacity.ts
@@ -1,6 +1,6 @@
 export class CapacityCalculation {
     calculate(demand: number, capacity: number): number {
-        if (capacity <=0) {
+        if ( capacity <=0 || !capacity) {
             throw new Error('Capacity must be positive')
         }
 
