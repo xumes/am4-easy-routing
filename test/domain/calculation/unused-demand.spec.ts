2ca0ca5a5b954fb45c78ab4d2403ddb5b1c9d0e4
Reginaldo Santos <reginaldo.santos@redspace.com>
2023-01-26 22:19:54 -0400

    test: ensure configuration calculates the correct values

---
+++ test/domain/calculation/unused-demand.spec.ts
@@ -0,0 +1,41 @@
+import { UnusedDemandCalculation } from "../../../src/domain/calculation/unused-demand"
+import { SeatCategory } from "../../../src/shared/seat-category"
+
+describe('Unused Demand Calculation', () => {
+    it('it should calculate the unused seats on success', () => {
+        const initialFreeSeats = 100
+        const seats = 20
+        const type = SeatCategory.ECONOMY
+
+        const unusedDemandCalculation = new UnusedDemandCalculation()
+        unusedDemandCalculation.setFreeSeats(initialFreeSeats)
+
+        const result = unusedDemandCalculation.calculate({seats, type})
+
+        expect(result).toBe(20)
+    })
+
+    it('it should calculate business type the unused seats on success', () => {
+        const initialFreeSeats = 100
+        const seats = 20
+        const type = SeatCategory.BUSINESS
+
+        const unusedDemandCalculation = new UnusedDemandCalculation()
+        unusedDemandCalculation.setFreeSeats(initialFreeSeats)
+        const result = unusedDemandCalculation.calculate({seats, type})
+
+        expect(result).toBe(60)
+    })
+
+    it('it should calculate first class type the unused seats on success', () => {
+        const initialFreeSeats = 100
+        const seats = 20
+        const type = SeatCategory.FIRST_CLASS
+
+        const unusedDemandCalculation = new UnusedDemandCalculation()
+        unusedDemandCalculation.setFreeSeats(initialFreeSeats)
+        const result = unusedDemandCalculation.calculate({seats, type})
+
+        expect(result).toBe(40)
+    })
+})
